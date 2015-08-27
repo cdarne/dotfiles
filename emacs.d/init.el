6ecac071f7cdd81e76ca2d61787cc31a8f12dd7e
Cedric Darne <cedric.darne@gmail.com>
2015-08-27 13:59:04 -0400

    Emacs and zsh stuff
    Updated go config for emacs
    Added a zsh function for updating stuff

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,3 +1,11 @@
+;;; init.el --- emacs config
+
+;;; Commentary:
+
+;; Does all the config for emacs
+
+;;; Code:
+
 (require 'package) ;; You might already have this line
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
@@ -20,22 +28,29 @@
  '(show-paren-mode t)
  '(tool-bar-mode nil))
 
-(ido-mode)
-(ido-everywhere t)
-(setq ido-enable-flex-matching t)
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(add-to-list 'load-path "~/.emacs.d/lisp/")
 
+(ido-mode)
+;;(ido-everywhere t)
+;;(setq ido-enable-flex-matching t)
+
+;; Go config
 (require 'go-mode-autoloads)
 (setq gofmt-command "goimports")
+(add-hook 'before-save-hook #'gofmt-before-save)
+(add-hook 'go-mode-hook (lambda ()
+                          (local-set-key (kbd "M-.") #'godef-jump)))
+
+(add-hook 'after-init-hook #'global-flycheck-mode)
+(require 'go-autocomplete)
+(require 'auto-complete-config)
+(ac-config-default)
+(define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
 
 ;; Tab/indentation config
-(setq tab-width 4)
-(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4
+              standard-indent 2
+              indent-tabs-mode nil)
 
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
@@ -44,6 +59,7 @@
 
 (global-set-key (kbd "C-x g") 'magit-status)
 
+;; org mode key bindings
 (global-set-key (kbd "C-c l") 'org-store-link)
 (global-set-key (kbd "C-c a") 'org-agenda)
 (global-set-key (kbd "C-c c") 'org-capture)
